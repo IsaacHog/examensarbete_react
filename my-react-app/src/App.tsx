92bc3284899ceff2783044cbd353a9c99f8dc8e1
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-05-09 10:17:06 +0200

    add my-fancy-forms

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,6 +1,7 @@
-import { VStack } from '@chakra-ui/react';
+import { HStack, VStack } from '@chakra-ui/react';
 import './App.css';
 import MyFancyButtonGroup from './components/my-fancy-button-group';
+import MyFancyForms from './components/my-fancy-forms';
 
 function App() {
   return (
@@ -9,6 +10,14 @@ function App() {
         <VStack>
           <MyFancyButtonGroup numButtons={5} />
           <MyFancyButtonGroup numButtons={2} />
+        </VStack>
+
+        <VStack>
+          <HStack>
+            <MyFancyForms numForms={1} />
+            <MyFancyForms numForms={1} />
+          </HStack>
+          <MyFancyForms numForms={2} />
         </VStack>
       </header>
     </div>
---
+++ my-react-app/src/components/my-fancy-forms.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Box, Flex, Button } from '@chakra-ui/react';
+
+type FormInputProps = {
+  label: string;
+  placeholder?: string;
+  type?: string;
+  onChange: (value: string) => void;
+};
+
+const FormInput: React.FC<FormInputProps> = ({ label, placeholder, type = 'text', onChange }) => {
+  const [value, setValue] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+    onChange(newValue);
+  };
+
+  return (
+    <Box>
+      <label>{label}</label>
+      <input type={type} placeholder={placeholder} value={value} onChange={handleInputChange} />
+    </Box>
+  );
+};
+
+type FormProps = {
+  numForms: number;
+};
+
+const MyFancyForms: React.FC<FormProps> = ({ numForms }) => {
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Form submitted!');
+  };
+
+  const handleInputChange = (formNum: number, inputValue: string) => {
+    console.log(`Form ${formNum} input changed to: ${inputValue}`);
+  };
+
+  const forms = [];
+
+  for (let i = 1; i <= numForms; i++) {
+    forms.push(
+      <Box key={i} borderWidth="1px" borderRadius="lg" overflow="hidden" p="4">
+        <form onSubmit={handleFormSubmit}>
+          <FormInput label={`Input ${i}`} onChange={(value) => handleInputChange(i, value)} />
+          <Flex justify="flex-end">
+            <Button type="submit" mt={4} colorScheme="teal">
+              Submit
+            </Button>
+          </Flex>
+        </form>
+      </Box>
+    );
+  }
+
+  return <>{forms}</>;
+};
+
+export default MyFancyForms;
