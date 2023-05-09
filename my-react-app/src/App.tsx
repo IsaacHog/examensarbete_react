f5f30209fa6e55a4163228043b88f02a42ffaa2f
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2023-05-09 10:24:28 +0200

    add stack

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { HStack, VStack } from '@chakra-ui/react';
+import { HStack, Stack, VStack } from '@chakra-ui/react';
 import './App.css';
 import MyFancyButtonGroup from './components/my-fancy-button-group';
 import MyFancyForms from './components/my-fancy-forms';
@@ -7,18 +7,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <VStack>
-          <MyFancyButtonGroup numButtons={5} />
-          <MyFancyButtonGroup numButtons={2} />
-        </VStack>
+        <Stack>
+          <VStack>
+            <MyFancyButtonGroup numButtons={5} />
+            <MyFancyButtonGroup numButtons={2} />
+          </VStack>
 
-        <VStack>
-          <HStack>
-            <MyFancyForms numForms={1} />
-            <MyFancyForms numForms={1} />
-          </HStack>
-          <MyFancyForms numForms={2} />
-        </VStack>
+          <VStack>
+            <HStack>
+              <MyFancyForms numForms={1} />
+              <MyFancyForms numForms={1} />
+            </HStack>
+            <MyFancyForms numForms={2} />
+          </VStack>
+        </Stack>
       </header>
     </div>
   );
